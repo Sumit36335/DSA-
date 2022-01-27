66d6ed54ed7b3cc48919096f5b77c106e96b539b
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-01-28 00:29:28 +0530

    Create 141. LinkList Cycle.java

---
+++ LinkList/LeetCode/141. LinkList Cycle.java
@@ -0,0 +1,26 @@
+public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null)
+        {
+            return false;
+        }
+        if(head.next==head)
+        {
+            return true;
+        }
+        else
+        {
+           ListNode slow=head;
+           ListNode fast=head;
+           while(fast!=null && fast.next!=null)
+           {
+              slow=slow.next;
+              fast=fast.next.next;
+               if(slow==fast)
+               {
+                  return true;  
+               }
+           }
+           return false;
+         }
+    }
+}

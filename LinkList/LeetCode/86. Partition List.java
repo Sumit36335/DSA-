e536107f604f054887467893745f2e16330745e6
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-01-28 00:27:35 +0530

    Create 86. Partition List.java

---
+++ LinkList/LeetCode/86. Partition List.java
@@ -0,0 +1,23 @@
+public ListNode partition(ListNode head, int x) {
+         ListNode lHead=new ListNode(-1);
+        ListNode lTail=lHead;
+        ListNode rHead=new ListNode(-1);
+        ListNode rTail=rHead;
+        while(head!=null)
+        {
+            if(head.val<x)
+            {
+                lTail.next=head;
+                lTail=head;
+            }
+            else
+            {
+                rTail.next=head;
+                rTail=head;
+            }
+            head=head.next;
+        }
+        lTail.next=rHead.next;
+        rTail.next=null;
+        return lHead.next;
+    }

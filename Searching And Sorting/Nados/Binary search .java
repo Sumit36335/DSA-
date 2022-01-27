59e74bbf1cfec756e1089aa5c3da70a7f8a303a5
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-01-27 23:44:07 +0530

    Create Binary search .java

---
+++ Searching And Sorting/Nados/Binary search .java
@@ -0,0 +1,20 @@
+ public int search(int[] nums, int target) {
+        
+        int left =0 ; 
+        int right = nums.length-1;
+        
+        for ( int i = 0 ; i< nums.length ; i++){
+            int mid = (left+right)/2;
+            
+            if ( nums[mid]==target){
+                return mid;
+            }else if ( nums[mid]>target){
+                right = mid-1;
+            }else {
+                left = mid+1;
+            }
+        }
+        
+        return -1;
+        
+    }

ccfc80caeb7d079195365eaa9eaa27e6f59d1cde
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-01-28 00:30:29 +0530

    Create 142. LinkList Cycle 2.java

---
+++ LinkList/LeetCode/142. LinkList Cycle 2.java
@@ -0,0 +1,25 @@
+public ListNode detectCycle(ListNode head) {
+           ListNode slow=head;
+           ListNode fast=head;
+           while(fast!=null && fast.next!=null)
+           {
+              slow=slow.next;
+              fast=fast.next.next;
+               if(slow==fast)
+               {
+                  break;
+               }
+           }
+           if(fast==null || fast.next==null)
+           {
+               return null;
+           }
+           ListNode curr1=head,curr2=slow;
+           while(curr1!=curr2)
+           {
+               curr1=curr1.next;
+               curr2=curr2.next;
+           }
+           return curr1;
+    }
+}

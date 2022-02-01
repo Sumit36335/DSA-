693dfb5774556b146fa77d924efda9b6684ba758
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-02-01 15:43:50 +0530

    Create LinkList to Queue Adapter.java

---
+++ Stack And Queues/Nados/LinkList to Queue Adapter.java
@@ -0,0 +1,37 @@
+ public static class LLToQueueAdapter {
+    LinkedList<Integer> list;
+
+    public LLToQueueAdapter() {
+      list = new LinkedList<>();
+    }
+
+    int size() {
+      // write your code here
+      return list.size();
+    }
+
+    void add(int val) {
+      // write your code here
+      list.addLast(val);
+    }
+
+    int remove() {
+      // write your code here
+      if(size()==0)
+      {
+        System.out.println("Queue underflow");
+        return -1;
+      }
+      return list.removeFirst();
+    }
+
+    int peek() {
+      // write your code here
+      if(size()==0)
+      {
+        System.out.println("Queue underflow");
+        return -1;
+      }
+      return list.getFirst();
+    }
+  }

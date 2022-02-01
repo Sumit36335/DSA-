83407bda5e34ef242c90b77abe75427a7740571a
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-02-01 15:42:00 +0530

    Create LinkList to Stack Adapter.java

---
+++ Stack And Queues/Nados/LinkList to Stack Adapter.java
@@ -0,0 +1,38 @@
+public static class LLToStackAdapter {
+    LinkedList<Integer> list;
+
+    public LLToStackAdapter() {
+      list = new LinkedList<>();
+    }
+
+
+    int size() {
+      // write your code here
+      return list.size();
+    }
+
+    void push(int val) {
+      // write your code here
+      list.addFirst(val);
+    }
+
+    int pop() {
+      // write your code here
+      if(size()==0)
+      {
+        System.out.println("Stack underflow");
+        return -1;
+      }
+      return list.removeFirst();
+    }
+
+    int top() {
+      // write your code here\
+      if(size()==0)
+      {
+        System.out.println("Stack overflow");
+        return -1;
+      }
+      return list.getFirst();
+    }
+  }

5cf0e84cbcf9621db152820d389d4eb56f79e8b5
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-02-11 11:07:52 +0530

    Create Sliding Window.java

---
+++ Stack And Queues/Nados/Sliding Window.java
@@ -0,0 +1,44 @@
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++){
+       arr[i] = Integer.parseInt(br.readLine());
+    }
+    int k = Integer.parseInt(br.readLine());
+
+    // code
+    Stack<Integer> stk= new Stack<>();
+
+    int [] nge= new int[arr.length];
+    stk.push(arr.length-1);
+    nge[arr.length-1]=arr.length;
+
+    for(int i=arr.length-2;i>=0;i--)
+    {
+       while(stk.size()!=0 && arr[i]>=arr[stk.peek()])
+       {
+          stk.pop();
+       }
+       if(stk.size()==0)
+       {
+          nge[i]=arr.length;
+       }
+       else
+       {
+          nge[i]=stk.peek();
+       }
+       stk.push(i);
+    }
+    int j =0;
+    for(int i=0;i<=arr.length-k;i++)
+    {
+       j=i;
+       while(nge[j]<i+k)
+       {
+          j=nge[j];
+       }
+       System.out.println(arr[j]);
+    }
+ }

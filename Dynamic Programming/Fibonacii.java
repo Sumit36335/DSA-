7655f842367f9c524e2ea5b9897f404f635068b5
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-04-20 00:03:01 +0530

    Update Fibonacii.java

--- Dynamic Programming/Fibonacii.java
+++ Dynamic Programming/Fibonacii.java
@@ -39,4 +39,18 @@ public static int fibTab(int n)
     }
     return dp[n];
 }
+ // space optimisation
+  public static int fibTab(int n)
+{
+    
+    prev1=0;
+    prev2=1;
+    for(int i=2;i<=n;i++)
+    {
+        int curr=prev1+prev2;
+        prev1=prev2;
+        prev2=curr;
+    }
+    return prev2;
+}
 }

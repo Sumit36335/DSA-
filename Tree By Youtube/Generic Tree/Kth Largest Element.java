a567aae892d874fbbed1149639da81d68ba237db
Sumit Singh <84323481+Sumit36335@users.noreply.github.com>
2022-05-25 01:18:11 +0530

    Create Kth Largest Element.java

---
+++ Tree By Youtube/Generic Tree/Kth Largest Element.java
@@ -0,0 +1,34 @@
+  
+  static int ceil;
+  static int floor;
+  public static void ceilAndFloor(Node node, int data) {
+    if(node.data > data){
+      if(node.data < ceil){
+        ceil = node.data;
+      }
+    }
+
+    if(node.data < data){
+      if(node.data > floor){
+        floor = node.data;
+      }
+    }
+
+    for (Node child : node.children) {
+      ceilAndFloor(child, data);
+    }
+  }
+
+  public static int kthLargest(Node node, int k){
+    
+    floor= Integer.MIN_VALUE;
+    int factor= Integer.MAX_VALUE;
+    
+    for(int i=0;i<k;i++)
+    {
+        ceilAndFloor(node,factor);
+        factor=floor;
+        floor=Integer.MIN_VALUE;
+    }
+    return factor;
+  }
